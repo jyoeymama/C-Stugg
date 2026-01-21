3de471d5051d36274eed7780a7a6f19dff633e0c
jyomama28 <jyomama28@gmail.com>
2026-01-21 16:37:38 -0500

    Add files via upload

---
+++ pointers.c
@@ -0,0 +1,9 @@
+// this is just some pointer practice for myself, I also need to practice my styling because right now its uGly
+#include <stdio.h>
+
+int main() {
+int prt = 69;
+int* ptr = &prt;
+printf("%d", ptr);
+return 0;
+}
