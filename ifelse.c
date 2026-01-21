f3609aad1e6b6f972ebffc92f142b917120d7741
jyomama28 <jyomama28@gmail.com>
2026-01-20 21:05:36 -0500

    Add files via upload

---
+++ ifelse.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+//#include <jyomama28.h>
+
+int main() {
+	int uno = 1;
+	int dos = 2;
+	if (uno == dos) {
+		printf("sigma");
+       	} else if (dos == uno) {
+	printf("sigma2");
+	}
+}
+
