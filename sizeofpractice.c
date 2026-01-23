60526385f048b22ea9b06f05aecbf2da1aeeaa00
jyomama28 <jyomama28@gmail.com>
2026-01-22 21:15:23 -0500

    Add files via upload

---
+++ sizeofpractice.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//#include <jyomama28isawesome.h>
+
+int main() {
+int num = 1;
+float num2 = 6.99;
+double num3 = 2.78;
+char letter = 'q';
+
+printf("%zu\n", num);
+printf("%zu\n", num2);
+printf("%zu\n", num3);
+printf("%zu\n", letter);
+printf("Above are some byte sizes, I am learning about the sizeof operator in C and how different byte sizes can effect memory as this can be used for optimization later which is SUPER COOL!!\n");
+}
